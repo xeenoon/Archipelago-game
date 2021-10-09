4b005c7a4594e6707ae4d8ebd86712212addaf59
Christopher <wha0011@mwsc.vic.edu.au>
2021-10-09 20:00:18 +1100

    Added a random move generator

--- Archipelago/Move.cs
+++ Archipelago/Move.cs
@@ -21,7 +21,7 @@ namespace Archipelago
         }
         public Move(Ship toMove, Point currentLocation, Point destination)
         {
-            this.toMove.Add(toMove); 
+            this.toMove.Add(toMove);
             this.currentLocation = currentLocation;
             this.destination = destination;
         }
@@ -34,6 +34,30 @@ namespace Archipelago
                 MainGameForm.squares[destination.X, destination.Y].ships.Add(ship); //Add the ship to its new destination
                 MainGameForm.RunAttack(MainGameForm.squares[destination.X, destination.Y]); //Attack the square
                 ship.hasMoved = false; //The ship has now moved, and cannot move again until the next turn
+            }
+        }
+        public static void DoRandomMove()
+        {
+            List<Square> teamSquares = new List<Square>();
+
+            foreach (var s in MainGameForm.squares)
+            {
+                if (s.GetTeam() == MainGameForm.hasTurn)
+                {
+                    teamSquares.Add(s);
+                }
+            } //Add all the ships on my team into the teamShips list
+            if (teamSquares.Count() == 0)
+            {
+
+            }
+            else
+            {
+                var squareToMoveFrom = teamSquares.Shuffle().FirstOrDefault(); //Choose a random ship
+                var destinations = Ship.Destinations(squareToMoveFrom.ships.FirstOrDefault().shipType, squareToMoveFrom.location); //Find the destinations possible for the first ship in the squareToMoveFrom ships
+                var randomDestination = destinations.Where(p=>MainGameForm.CanMove(p.X, p.Y)).ToList().Shuffle().FirstOrDefault(); //Choose a random destination that we can move too
+                Move randomMove = new Move(squareToMoveFrom.ships.FirstOrDefault(), squareToMoveFrom.location, randomDestination);
+                randomMove.DoMove();
             }
         }
     }
