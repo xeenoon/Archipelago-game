47c1791fb64774193d05b0e8973e23d9f2423d31
Christopher <wha0011@mwsc.vic.edu.au>
2021-10-13 17:13:57 +1100

    Made AI more conservative

--- Archipelago/Rule.cs
+++ Archipelago/Rule.cs
@@ -16,11 +16,14 @@ namespace Archipelago
             this.ID = ID;
             this.Condition = Condition;
             this.Reaction = Reaction;
+
+            rules.Add(this);
         }
+        public static List<Rule> rules = new List<Rule>(); 
 
         //Default defined rules
         public static Rule AttackEverything = new Rule(0, RuleFunctions.CanAttack, RuleFunctions.AttackFirst);
-        public static Rule DefendPort = new Rule(0, RuleFunctions.IsEmptyPort, RuleFunctions.BuildShipInPort);
+        public static Rule DefendPort = new Rule(0, RuleFunctions.IsVulnerablePort, RuleFunctions.BuildShipInPort);
     }
     public static class RuleFunctions
     {
@@ -37,7 +40,8 @@ namespace Archipelago
                         var positions = Ship.Destinations(ship.shipType, square.location).Where(p => MainGameForm.CanMove(p.X, p.Y)); //Figure out the possible moves the ship cam make
                         foreach (var p in positions)
                         {
-                            if (MainGameForm.squares[p.X, p.Y].GetTeam() != hasTurn && MainGameForm.squares[p.X, p.Y].GetTeam() != Team.None) //Can it attack?
+                            var targetTeam = MainGameForm.squares[p.X, p.Y].GetTeam();
+                            if (targetTeam != hasTurn && targetTeam != Team.None && targetTeam != Team.Pirate) //Can it attack, and never attack a pirate
                             {
                                 return true; //We can attack a square
                             }
@@ -74,26 +78,43 @@ namespace Archipelago
         }
 
         //Defend port functions
-        public static bool IsEmptyPort()
+        public static bool IsVulnerablePort()
         {
             Team hasTurn = MainGameForm.hasTurn;
             foreach (var square in MainGameForm.squares)
             {
                 if (square.isPort && square.team == hasTurn) //Is it one of my ports
                 {
-                    if (square.ships.Count() == 0) //Are there no ships defending the port
+                    if (square.ships.Count() != 0) //Does it already have a guard
                     {
-                        return true;
+                        continue; //Ignore it, we only ever need one guard
+                    }
+                    //Record the positions of everything that is close enough to move to us
+                    int left4  = square.location.X - 4 < 0 ? 0 : square.location.X - 4; //The furtherest left we can go
+                    int down4 = square.location.Y - 4 < 0 ? 0 : square.location.Y - 4; //The furtherest right we can go
+
+                    int right4 = square.location.X + 4 > MainGameForm.horizontalSquares-1 ? MainGameForm.horizontalSquares-1: square.location.X + 4; //The furtherst right we can go
+                    int up4    = square.location.Y + 4 > MainGameForm.verticalSquares ? MainGameForm.verticalSquares : square.location.Y + 4; //The furtherest up we can go
+
+                    for (int x = left4; x < right4; ++x) //Go from the furtherest left to the furtherest right
+                    {
+                        for (int y = down4; y < up4; ++y) //Go from the furtherest down to the furtherest up
+                        {
+                            if (MainGameForm.squares[x,y].ships.Count() != 0) //Is there 1 or more ships in the square
+                            {
+                                return true; //A ship is within moving distance of our port
+                            }
+                        }
                     }
                 }
             }
-            return false; //All ports are defended
+            return false; //Ports are not in danger
         }
         public static List<Move> BuildShipInPort()
         {
             List<Move> movesToMake = new List<Move>();
             Team hasTurn = MainGameForm.hasTurn;
-            var ship = Ship.BuildShipInBudget(MainGameForm.teamMaterials.GetMaterials(hasTurn)); //Build a random ship
+            var ship = Ship.BuildBiggestShipInBudget(MainGameForm.teamMaterials.GetMaterials(hasTurn), Ship.ShipType.Heavy); //Build a random heavy ship
             if (ship != null) //Make sure we have enough materials to build the ship
             {
                 foreach (var square in MainGameForm.squares) 
